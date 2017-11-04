f2c4dfc0e9f1a5c4e1ba1aa1c209c9a5436c04d6
Frode Hus <frodehus@outlook.com>
2017-11-04 16:01:01 +0100

    added query by time

--- plugins/logstash-input-appinsights/lib/logstash/inputs/appinsights.rb
+++ plugins/logstash-input-appinsights/lib/logstash/inputs/appinsights.rb
@@ -5,20 +5,22 @@ require "stud/interval"
 require "socket" # for Socket.gethostname
 require "json"
 require "rest-client"
+require "active_support/all"
+require "uri"
 
 class LogStash::Inputs::Appinsights < LogStash::Inputs::Base
   config_name "appinsights"
-
   default :codec, "json"
 
   config :apps, :validate => :hash, :required => true
+  config :base_url, :default => "https://api.applicationinsights.io/v1/apps"
 
   config :interval, :validate => :number, :default => 60
-
+  config :start_from_days, :validate => :number, :default => 1
   public
   def register
     @host = Socket.gethostname
-  end 
+  end
 
   def run(queue)
     while !stop?
@@ -27,48 +29,81 @@ class LogStash::Inputs::Appinsights < LogStash::Inputs::Base
           api_key = @apps[appName]["key"]
           app_id = @apps[appName]["id"]
           source = @apps[appName]["source"]
-          response = RestClient.get("https://api.applicationinsights.io/v1/apps/#{app_id}/query?query=#{source}%7C%20where%20timestamp%20%3E%3D%20ago(5m)", :'x-api-key' => api_key)
-          tabledata = JSON[response]
-          parse_ai_data(appName, tabledata, queue)
+	  process(app_id, api_key, source, appName, queue)
         end
       end
       Stud.stoppable_sleep(@interval) { stop? }
     end # loop
   end # def run
 
-  def parse_ai_data(appName, data, queue)
-   if data.nil?
-     return
-   end
-   tables = data["tables"]
-   tables.each do |table|
-     cols = table["columns"]
-     rows = table["rows"]
-     rows.each do |row|
-       e = parse_event(row, cols)
-       next if e.nil?
-       e["application"] = appName
-       @codec.decode(e.to_json()) do |event|
-         decorate(event)
-         event.set("@timestamp", LogStash::Timestamp.new(Time.parse(e["timestamp"])))
-         queue << event
-       end
-     end
-   end
+  def get_ai_data(app_id, key, source, time)
+	uri = "#{@base_url}/#{app_id}/query?timespan=#{time}&query=#{source}| order by timestamp asc"
+	@logger.error(uri)
+	@logger.error(URI.escape(uri))
+        response = RestClient.get(URI.escape(uri), :'x-api-key' => key, :accept => "application/json")
+        tabledata = JSON[response]
+        return tabledata
   end
 
-  def parse_event(row, cols)
-   if cols.nil?
-     return nil
-   end
-   e = Hash.new()
-   index = 0
-   cols.each do |col|
-     e[col["name"]] = row[index]
-     index += 1
-   end
-   return e
+  def process(app_id, key, source, appName, queue)
+        for time in get_time_slices(@start_from_days.days.ago)
+                start = time.strftime("%Y-%m-%d %H:%M")
+                end_date = (time + 1.hour).strftime("%Y-%m-%d %H:%M")
+                data = get_ai_data(app_id, key, source, start + "/" + end_date)
+                parse_data(data, appName, queue)
+        end
   end
+
+  def parse_data(data, appName)
+        if data.nil?
+                return
+        end
+        tables = data["tables"]
+        if tables.nil?
+                return
+        end
+
+        tables.each do |table|
+                cols = table["columns"]
+                rows = table["rows"]
+                ts = nil
+                rows.each do |row|
+                        e = parse_row(row, cols)
+                        next if e.nil?
+                        ts = Time.parse(e["timestamp"])
+                        e["application"] = appName
+                        @codec.decode(e.to_json()) do |event|
+                        	decorate(event)
+                        	event.set("@timestamp", LogStash::Timestamp.new(ts))
+                        	queue << event
+			end
+                end
+        end
+  end
+
+  def parse_row(row, cols)
+        if cols.nil?
+                return nil
+        end
+
+        e = Hash.new
+        index = 0
+        cols.each do |col|
+                e[col["name"]] = row[index]
+                index += 1
+        end
+        return e
+  end
+
+  def get_time_slices(start)
+        range = (start.to_i..Time.now.to_i).step(1.hour)
+        slices = Array.new
+        range.each do |time|
+                slices <<  Time.at(time).to_datetime
+        end
+        return slices
+  end
+
 
   def stop
   end
--- plugins/logstash-input-appinsights/logstash-input-appinsights.gemspec
+++ plugins/logstash-input-appinsights/logstash-input-appinsights.gemspec
@@ -3,8 +3,8 @@ Gem::Specification.new do |s|
   s.version       = '0.1.0'
   s.licenses      = ['Apache License (2.0)']
   s.summary       = 'Reads data from Application Insights REST Api.'
-  s.homepage      = 'https://github.com'
-  s.authors       = ['Frode Hus']
+  s.homepage      = 'https://github.com/Statoil/logstash'
+  s.authors       = ['Statoil']
   s.email         = 'frodehus@outlook.com'
   s.require_paths = ['lib']
 
@@ -17,10 +17,10 @@ Gem::Specification.new do |s|
   s.metadata = { "logstash_plugin" => "true", "logstash_group" => "input" }
 
   # Gem dependencies
-  s.add_runtime_dependency "logstash-core-plugin-api", "~> 2.0"
-  s.add_runtime_dependency 'logstash-codec-plain'
+  s.add_runtime_dependency "logstash-core-plugin-api", '>= 1.60', '<= 2.99'
   s.add_runtime_dependency 'logstash-codec-json'
   s.add_runtime_dependency 'stud', '>= 0.0.22'
   s.add_development_dependency 'logstash-devutils', '>= 0.0.16'
   s.add_runtime_dependency 'rest-client'
+  s.add_runtime_dependency 'activesupport', '3.2'
 end
